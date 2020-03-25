e7b17eb640ce8aceed0f5110ef3d9766e509f4c1
Jan Krupa <jan.krupa@vsb.cz>
2020-03-25 16:42:34 +0100

    Add form for creating new event into list
    - install axios js package
    - add create action to events controller
    - add events into routes
    - Eventlite js manages own state, for rendering newly added event
    - EventForm js manages own state, for updating input values
    - Data from EventForm are passed to Eventlite via function passed as prop

--- app/javascript/packs/eventlite.js
+++ app/javascript/packs/eventlite.js
@@ -2,13 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import EventsList from './events_list'
+import EventForm from './event_form'
 
-const Eventlite = props => (
-  <div>
-    <h1>Eventlite</h1>
-    <EventsList events={props.events}/>
-  </div>
-)
+class Eventlite extends React.Component {
+  // load state from pros
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      events: this.props.events
+    }
+  }
+
+  // set new event to a state
+  addNewEvent = (event) => {
+    const events = [event, ...this.state.events].sort(function (a, b) {
+      return new Date(a.start_datetime) - new Date(b.start_datetime)
+    })
+    this.setState({events: events})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Eventlite</h1>
+        {/* passes function addNewEvent as prop to event_form#HandleSubmit for use --> */}
+        <EventForm handleNewEvents={this.addNewEvent} />
+        <EventsList events={this.state.events}/>
+      </div>
+    )
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('events_data')
