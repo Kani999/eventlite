582e4109ab06ad84391da9399c2c828f147bbc3f
Jan Krupa <jan.krupa@vsb.cz>
2020-03-26 10:38:31 +0100

    Lifting state up
    - State from EventForm for handling inputs moved to Eventlite
    - Class EventForm changed to a const
    - Values and functions for handling inputs passed as props to form

--- app/javascript/packs/event_form.js
+++ app/javascript/packs/event_form.js
@@ -1,62 +1,16 @@
 import React from 'react'
-import axios from 'axios'
 
 // constructor for inputs
-class EventForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: '',
-            start_datetime: '',
-            location: ''
-        }
-    }
-
-    // set state when inputs are modified
-    handleInput = (event) => {
-        const name = event.target.name;
-        const newState = {};
-        newState[name] = event.target.value;
-        this.setState(newState)
-        event.preventDefault();
-    }
-
-    // sent data to CREATE events controlelr
-    handleSubmit = e => {
-        const handleNewEvent = this.props.handleNewEvents
-
-        axios({
-            method: 'POST',
-            url: 'events',
-            data: { event: this.state },
-            headers: {
-                'X-CSRF-Token': document.querySelector("meta[name=csrf-token").content
-            }
-        })
-        .then(function (response) {
-            // when response returns data, sent it as props to Eventlite
-            console.log(response)
-            handleNewEvent(response.data)
-        })
-        .catch(function(error) {
-            console.log(error)
-        })
-        e.preventDefault()
-    }
-
-    render() {
-        return (
-            <div>
-                <h4>Create an Event:</h4>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleInput} />
-                    <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime} onChange={this.handleInput} />
-                    <input type="text" name="location" placeholder="Location" value={this.state.location} onChange={this.handleInput} />
-                    <button type="submit">Create Event</button>
-                </form>
-            </div>
-        )
-    }
-}
+const EventForm = (props) => (
+    <div>
+        <h4>Create an Event:</h4>
+        <form onSubmit={props.handleSubmit}>
+            <input type="text" name="title" placeholder="Title" value={props.title} onChange={props.handleInput} />
+            <input type="text" name="start_datetime" placeholder="Date" value={props.start_datetime} onChange={props.handleInput} />
+            <input type="text" name="location" placeholder="Location" value={props.location} onChange={props.handleInput} />
+            <button type="submit">Create Event</button>
+        </form>
+    </div>
+)
 
 export default EventForm
--- app/javascript/packs/eventlite.js
+++ app/javascript/packs/eventlite.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
+
 
 import EventsList from './events_list'
 import EventForm from './event_form'
@@ -10,8 +12,45 @@ class Eventlite extends React.Component {
     super(props)
 
     this.state = {
-      events: this.props.events
+      //events displayed at the page
+      events: this.props.events,
+      // inputs for EventForm
+      title: '',
+      start_datetime: '',
+      location: ''
+
     }
+  }
+
+  // set state and sent it as props to EventForm
+  handleInput = (event) => {
+    const name = event.target.name;
+    const newState = {};
+    newState[name] = event.target.value;
+    this.setState(newState)
+    event.preventDefault();
+  }
+
+    // sent data to CREATE events controlelr
+  handleSubmit = e => {
+    axios({
+      method: 'POST',
+      url: 'events',
+      data: { event: this.state },
+      headers: {
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token").content
+      }
+    })
+    .then(function (response) {
+      console.log(response)
+      // pass data to a list of events
+      this.addNewEvent(response.data)
+    }.bind(this))
+    .catch(function(error) {
+      console.log(error)
+    })
+
+    e.preventDefault()
   }
 
   // set new event to a state
@@ -27,7 +66,12 @@ class Eventlite extends React.Component {
       <div>
         <h1>Eventlite</h1>
         {/* passes function addNewEvent as prop to event_form#HandleSubmit for use --> */}
-        <EventForm handleNewEvents={this.addNewEvent} />
+        <EventForm 
+          handleSubmit = {this.handleSubmit}
+          handleInput={this.handleInput}
+          title = {this.state.title}
+          start_datetime={this.state.start_datetime}
+          location={this.state.location} />
         <EventsList events={this.state.events}/>
       </div>
     )
